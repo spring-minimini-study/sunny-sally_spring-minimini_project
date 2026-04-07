cd4c0179a5811ea1cf1c2671c02975c6f34e20d5
김경민 <sally0264@naver.com>
2026-04-07 20:04:45 +0900

    fix: 갈아엎기

--- src/main/java/com/example/mini_spring/domain/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mini_spring.domain.user.repository;
-
-import com.example.mini_spring.domain.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
