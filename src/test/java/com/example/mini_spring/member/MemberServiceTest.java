84a08b06c5bd76246bcb4239f1d72198362239bc
김경민 <sally0264@naver.com>
2026-04-08 11:59:55 +0900

    fix: 260408.md 수정

---
+++ src/test/java/com/example/mini_spring/member/MemberServiceTest.java
@@ -0,0 +1,9 @@
+package com.example.mini_spring.member;
+
+import com.example.mini_spring.member.service.MemberService;
+
+public class MemberServiceTest {
+
+
+    MemberService memberservice = new MemberService();
+}
